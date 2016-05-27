b1a0b7f1f39ae75b50805e18cfdabb8ef8782674
smartyad4 <smarty.ad4@gmail.com>
2016-05-27 08:25:11 +0530

    Initial Commit Liferay 7 portlet

---
+++ FirstModuleProject/bin/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="firstmoduleproject_FirstModuleProjectmvcportletPortlet.caption"/></b>
+</p>
---
+++ FirstModuleProject/src/main/java/com/example/FirstModuleProjectmvcportletPortlet.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=FirstModuleProject Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class FirstModuleProjectmvcportletPortlet extends MVCPortlet {
+	@Override
+	public void render(RenderRequest renderRequest, RenderResponse renderResponse)
+			throws IOException, PortletException {
+		System.out.println("LR 7 Render Method");
+		super.render(renderRequest, renderResponse);
+	}
+}
